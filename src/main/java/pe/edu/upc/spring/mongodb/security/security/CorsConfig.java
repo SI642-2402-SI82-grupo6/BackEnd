b20e03e63cbe8de9fb659e14c6fa49cd1b30c5cb
Sergio Pecan <u20201a938@upc.edu.pe>
2024-10-30 13:38:00 -0500

    completed

---
+++ src/main/java/pe/edu/upc/spring/mongodb/security/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.spring.mongodb.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("*") // Allow all methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
