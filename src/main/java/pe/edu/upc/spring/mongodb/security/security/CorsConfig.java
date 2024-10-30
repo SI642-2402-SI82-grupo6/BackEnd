ec518be30af6eece72f0c1aa601ee59c53e04eae
Sergio Pecan <u20201a938@upc.edu.pe>
2024-10-30 13:54:02 -0500

    completed

--- src/main/java/pe/edu/upc/spring/mongodb/security/security/CorsConfig.java
+++ src/main/java/pe/edu/upc/spring/mongodb/security/security/CorsConfig.java
@@ -2,23 +2,22 @@ package pe.edu.upc.spring.mongodb.security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173") // Allow specific origin
-                        .allowedMethods("*") // Allow all methods
-                        .allowedHeaders("*") // Allow all headers
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
